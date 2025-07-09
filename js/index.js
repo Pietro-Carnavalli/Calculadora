c05fc57be8ab6377395b87e1e01f0713a75ba729
Pietrin <carnavallip@gmail.com>
2025-07-09 19:55:24 -0300

    'Pronto'

--- js/index.js
+++ js/index.js
@@ -1,10 +1,32 @@
-function calc(){
-    let number = document.querySelector('btn-number').value;
-    let operator = document.querySelector('btn-operator').value;
-    let equal = document.querySelector('btn-equal');
-    let deletebtn = document.querySelector('btn-delete');
-    let clear = document.querySelector('btn-clear');
 
-    let result = document.getElementById('display');
+function inserir(valor) {
+    const display = document.getElementById('display');
+    if (display.innerText === '0' || displey.innerText === 'Error') {
+        display.innerText = valor;
+    } else {
+        display.innerText+= valor;
+    }
+}
 
+function calcular() {
+    const displey = document.getElementById('display');
+    try {
+        const resultado = eval(displey.value);
+        displey.innerText = resultado;
+    } catch {
+        displey.innerText = 'Error';
+    }
+}
+
+function limpar() {
+    document.getElementById("display").innerText = "0";
+}
+
+function apagar() {
+    const displey = document.getElementById('display');
+    if (displey.value.length > 1) {
+        displey.innerText = displey.value.slice(0, -1);
+    } else {
+        displey.innerText= '0';
+    }
 }
